c5246797b4253cd9aa5c86e9a2e14cf0d0c84af8
Sage <otun.ibrahim.yinka@gmail.com>
2025-05-25 22:52:26 +0100

    Performing basic arithmetics

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> number1 = 10
+>>> number2 = 5
+>>> 
+>>> addition = number1 + number2
+>>> subtraction = number1 - number2
+>>> multiplication = number1*number2
+>>> 
+>>> print(f"addition of {number1} and {number2} is {addition}")
+addition of 10 and 5 is 15
+>>> print(f"subtraction of {number1} and {number2} is {subtraction}")
+subtraction of 10 and 5 is 5
+>>> print(f"multiplication of {number1} and {number2} is {multiplication}")
+multiplication of 10 and 5 is 50

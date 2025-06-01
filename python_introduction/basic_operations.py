1e954a0bb6f5cd3423297e692cbedd1c862f51d5
Sage <otun.ibrahim.yinka@gmail.com>
2025-06-01 23:27:25 +0100

    Updated tasks

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,12 +1,11 @@
-Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> number1 = 10
->>> number2 = 5
->>> 
->>> addition = number1 + number2
->>> subtraction = number1 - number2
->>> multiplication = number1*number2
->>> 
->>> print(f"addition of {number1} and {number2} is {addition}")
->>> print(f"subtraction of {number1} and {number2} is {subtraction}")
->>> print(f"multiplication of {number1} and {number2} is {multiplication}")
+
+number1 = 10
+number2 = 5
+ 
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1*number2
+ 
+print(f"addition of {number1} and {number2} is {addition}")
+print(f"subtraction of {number1} and {number2} is {subtraction}")
+print(f"multiplication of {number1} and {number2} is {multiplication}")

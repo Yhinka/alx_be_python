14b82be3c3e69cb01e304750dc3ac7798ccde964
Sage <otun.ibrahim.yinka@gmail.com>
2025-06-01 22:38:38 +0100

    All tasks completed done

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,17 +1,12 @@
 # pattern_drawing.py
 
-
-def draw_square_pattern(size):
-    for i in range(size):
-        yield "* " * size
-
 while True:
-    try:
-        size = int(input("Enter the size of the pattern (positive integer): "))
-        if size > 0:
-            draw_square_pattern(size)
-            break
-        else:
-            print("Please enter a positive integer.")
-    except ValueError:
-        print("Invalid input. Please enter a positive integer.")
+    size = int(input("Enter the size of the pattern (positive integer): "))
+    if size > 0:
+        def draw_square_pattern(size):
+            for i in range(size):
+                print("* " * size)
+        draw_square_pattern(size)
+        break
+    else:
+        print("Please enter a positive integer.")

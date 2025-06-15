64112e9c1b185225391f73d9e3a73c18bf9ea64d
Sage <otun.ibrahim.yinka@gmail.com>
2025-06-15 21:58:10 +0100

    Updated tasks

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -7,7 +7,7 @@ def display_current_datetime():
 
 def calculate_future_date():
     try:
-        days = int(input("Enter number of days to add: "))
+        days = int(input("Enter the number of days to add to the current date: "))
     except ValueError:
         print("Please enter a valid integer.")
         return
